4a78f484924429f5687efb6abbe1351384fd5f89
Cole <eloc49@gmail.com>
2019-08-21 09:34:06 -0400

    made bib number assignment use pandas

--- assign_bib_numbers.py
+++ assign_bib_numbers.py
@@ -1,17 +1,10 @@
 import sys
-import csv
+import pandas as pd
 
 filepath = sys.argv[1]
 out_filepath = filepath.replace(".csv", "-with-bib-numbers.csv")
 
-with open(filepath, newline='', encoding='utf-8-sig') as csvin:
-    # BikeReg exports , delimited with "" quotecars
-    reader = csv.reader(csvin.readlines(), delimiter=',', quotechar='"')
+registrants = pd.read_csv(filepath)
+registrants['Bib'] = range(1, 1 + len(registrants))
+registrants.to_csv(out_filepath)
 
-with open(out_filepath, 'w', newline='', encoding='utf-8-sig') as csvout:
-    writer = csv.writer(csvout, dialect='excel')
-    for i, row in enumerate(reader):
-        new_row = row
-        if i > 0:
-            new_row[0] = i
-        writer.writerow(new_row)
