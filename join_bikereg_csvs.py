e72048f4108f61f2990cb9aff3f223b6ea65fa78
Cole <eloc49@gmail.com>
2019-08-26 08:32:20 -0400

    finished joining script

--- join_bikereg_csvs.py
+++ join_bikereg_csvs.py
@@ -1,11 +1,32 @@
 import os
 import sys
 
-pre_reg_filepath = sys.argv[1]
-walk_up_filepath = sys.argv[2]
-out_filepath = os.path.dirname(pre_reg_filepath) + '/' + \
-               os.path.basename(pre_reg_filepath).split("-")[0] + \
+import pandas as pd
+
+pre_reg_with_bib_numbers_filepath = sys.argv[1]
+with_walk_up_filepath = sys.argv[2]
+out_filepath = os.path.dirname(pre_reg_with_bib_numbers_filepath) + '/' + \
+               os.path.basename(pre_reg_with_bib_numbers_filepath).split("-")[0] + \
                '-all-reg.csv'
 
+pre_reg_df = pd.read_csv(pre_reg_with_bib_numbers_filepath)
+walk_up_df = pd.read_csv(with_walk_up_filepath)
+joined_df = pd.merge(
+    pre_reg_df, walk_up_df,
+    on=['First Name', 'Last Name', 'Category Entered/Merchandise Ordered'],
+    how='right'
+)
+joined_df.drop(
+    columns=['Bib_y'],
+    inplace=True
+)
+joined_df.rename(
+    columns={'Bib_x': 'Bib'},
+    inplace=True
+)
+joined_df['Bib'] = joined_df['Bib'].astype('Int32')
 
-print(out_filepath)
+joined_df.to_csv(
+    out_filepath,
+    index=False
+)
