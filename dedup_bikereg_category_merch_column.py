0c686545a29841e59a41387292642aef3c5b85f9
Cole <eloc49@gmail.com>
2019-10-01 10:49:56 -0400

    got basic script for usac results up

--- dedup_bikereg_category_merch_column.py
+++ dedup_bikereg_category_merch_column.py
@@ -7,12 +7,13 @@ bikereg_results_path = sys.argv[1]
 total_racers = int(sys.argv[2])
 no_merch_path = os.path.splitext(bikereg_results_path)[0] + \
                 '-no-merch.csv'
+cat_and_merch = 'Category Entered / Merchandise Ordered'
 
 bikereg_results_df = pd.read_csv(bikereg_results_path)
 
 
 def is_not_merch(row):
-    cat = row['Category Entered / Merchandise Ordered']
+    cat = row[cat_and_merch]
     is_merch = cat.startswith('T shirt') or \
                cat.startswith('Trail Maintenance Donations Welcomed')
     return not is_merch
@@ -27,8 +28,17 @@ no_merch_df = bikereg_results_df[no_merch_df]
 try:
     assert no_merch_df.shape[0] == total_racers
 except AssertionError as ass_err:
-    ass_err.args += (total_racers, 'no_merch_df has less than total_racers!')
+    ass_err.args += (
+        'DataFrame rows: ',
+        no_merch_df.shape[0],
+        'has less than total racers arg: ',
+        total_racers
+    )
     raise
+
+no_merch_df = no_merch_df.rename(columns={
+    cat_and_merch: 'Category Entered'
+})
 
 no_merch_df.to_csv(
     no_merch_path,
